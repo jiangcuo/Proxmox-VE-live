e82a774c7cd023434ea6efc87630d2d91030068e
jiangcuo <49061187+jiangcuo@users.noreply.github.com>
2023-02-01 20:26:43 +0800

    ad

--- scripts/initrd.img.sh
+++ scripts/initrd.img.sh
@@ -2,16 +2,19 @@
 arch="amd64"
 release="bullseye"
 rootfssrc="/tmp/rootfssrc"
-localfile="/root/scripts/local"
+localfile="/root/local"
 
 mv $rootfssrc/boot /tmp
 pve_initrd=`ls /tmp/boot/initrd*|head -n 1`
+rm /tmp/boot/initrd.img.gz 
+rm /tmp/boot/initrd.img
 cp $pve_initrd /tmp/boot/initrd.img.gz 
 mkdir /tmp/initrd
 cd /tmp/initrd
 gzip -d /tmp/boot/initrd.img.gz 
 cpio -idmv < /tmp/boot/initrd.img
 cp $localfile /tmp/initrd/scripts/
+rm $pve_initrd
 find . | cpio -o -H newc | gzip > $pve_initrd
 mkdir /tmp/iso
 mkdir /tmp/iso/{.installer,.installer-mp,.workdir,.base}
