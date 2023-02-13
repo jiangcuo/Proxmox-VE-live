090266d56d1366c59cb435a2e335e913ca8d8f41
jiangcuo <49061187+jiangcuo@users.noreply.github.com>
2023-02-13 12:02:52 +0800

    add rc-local

---
+++ etc/pvelive/hosts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+hostname=`sed "s/ /\n/g" /proc/cmdline |grep hostname`
+dn=`sed "s/ /\n/g" /proc/cmdline |grep dn`
+if [ ! -z $hostname ];then
+	export $hostname
+	echo "define the hostname,do config hostname"
+	echo $hostname >/etc/hostname
+    sed -i "s/pve/$hostname/g" /etc/hosts
+	systemctl set-hostname $hostname
+fi
+
+if [ ! -z $dn ];then
+	export $dn
+	echo "define the dn,do config dn"
+	sed -i "s/testlive.com/$dn/g" /etc/hosts
+fi
+
+exit 0
